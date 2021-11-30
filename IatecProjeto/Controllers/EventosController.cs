67f627d47d5e8cdcb3da2b14589c848046e28f50
Antonio Bianco <abqueiroz86@gmail.com>
2021-11-30 14:33:54 -0300

    Inserir eventosController

---
+++ IatecProjeto/Controllers/EventosController.cs
@@ -0,0 +1,26 @@
+﻿using IatecProjeto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IatecProjeto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventosController
+    {
+        /// <summary>
+        /// Insere usuário
+        /// </summary>
+        [HttpPost]
+        public Evento Add(Evento evento)
+        {
+            using (var contexto = new ProjetoContext())
+            {
+                contexto.Add(evento);
+                contexto.SaveChanges();
+
+                return evento;
+            }
+        }
+    }
+}
