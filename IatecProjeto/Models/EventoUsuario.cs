73dbd7195ee7df22b8378dabd6f38b73ed01014b
Antonio Bianco <abqueiroz86@gmail.com>
2021-11-30 18:23:32 -0300

    Remoção comentario

--- IatecProjeto/Models/EventoUsuario.cs
+++ IatecProjeto/Models/EventoUsuario.cs
@@ -4,7 +4,6 @@
     {
         public int UsuarioId { get; set; }
         public int EventoId { get; set; }
-        //public Usuario Usuario { get; set; }
         public Evento Evento { get; set; }
     }
 }
